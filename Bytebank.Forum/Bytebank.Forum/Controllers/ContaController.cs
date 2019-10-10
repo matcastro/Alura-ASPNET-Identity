92401c5f7843f6ee2ca5d6865ea6af072a56a20f
Mateus Castro <matcastro>
2019-10-09 23:53:50 -0300

    Criação do projeto e da primeira página com formulário de cadastro.

---
+++ Bytebank.Forum/Bytebank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,23 @@
+﻿using Bytebank.Forum.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bytebank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
---
+++ Bytebank.Forum/Bytebank.Forum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bytebank.Forum.MvcApplication" Language="C#" %>
