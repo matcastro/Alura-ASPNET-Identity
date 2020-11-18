4441665063bdc95f57e9419a41e88af6f289dd73
Mateus Castro <matcastro>
2020-11-18 20:39:45 -0300

    Incluindo email de servico para implementar SendAsync

---
+++ ByteBank.Forum/ByteBank.Forum/App_Start/Identity/EmailServico.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.App_Start.Identity
+{
+    public class EmailServico : IIdentityMessageService
+    {
+        private readonly string EMAIL_ORIGEM = ConfigurationManager.AppSettings["email_servico:email_remetente"];
+        private readonly string EMAIL_SENHA = ConfigurationManager.AppSettings["email_servico:email_senha"];
+        public async Task SendAsync(IdentityMessage message)
+        {
+            
+        }
+    }
+}
